5d471a7723b9d4065bd503c53dfbd11df73d6590
Captain <cato@nyu.edu>
2015-11-15 21:52:43 -0500

    Pressure: added P vs alpha functionality.

--- LE_LightBoundarySim.py
+++ LE_LightBoundarySim.py
@@ -22,9 +22,11 @@ def main():
 		
 		
 	FLAGS
-		-a --alpha		alpha
-		-r --nruns		number of runs for each (x0,y0)
-		-v --verbose
+		-a --alpha		0.1		Slope of the potential
+		-r --nruns		100		Number of runs for each (x0,y0)
+		-t --timefac	1.0		Multiply t_max by factor
+		-v --verbose	False	Print useful information to screen
+		-h --help		False	Print docstring and exit
 	
 	EXAMPLE
 		python LE_LightBoundarySim.py -a 0.5 -r 100 -v
@@ -38,22 +40,28 @@ def main():
 		13 November 2015	Adapted from LE_BoundarySimPlt.py
 	"""	
 	
-	me = "LE_BoundarySimPlt.main: "
+	me = "LE_LightBoundarySim.main: "
 	t0 = time.time()
 	
 	## ----------------------------------------------------------------
 	## Input options
 	
-	parser = optparse.OptionParser()	
+	parser = optparse.OptionParser(conflict_handler="resolve")	
 	parser.add_option('-a','--alpha',
-                  dest="potmag",default=0.1,type="float")
+                  dest="potmag",default=0.1,type="float",
+				  help="The steepness of the potential.")
 	parser.add_option('-r','--nrun',
                   dest="Nrun",default=100,type="int")		
 	parser.add_option('-t','--timefac',
                   dest="timefac",default=1.0,type="float")	  
 	parser.add_option('-v','--verbose',
-                  dest="verbose",default=False,action="store_true")					  
+                  dest="verbose",default=False,action="store_true",
+				  help="Print useful information to screen.")
+	parser.add_option('-h','--help',
+                  dest="help",default=False,action="store_true",
+				  help="Print docstring.")					  
 	opt = parser.parse_args()[0]
+	if opt.help: print main.__doc__; return
 	a		= opt.potmag
 	X		= 1.0
 	Nrun	= opt.Nrun
@@ -71,22 +79,24 @@ def main():
 	global dt; dt = 0.02
 	
 	## Histogramming
-	xmax = 1.1*X	## Should depend on b
+	xmax = calculate_xmax(X,a)
 	xmin = 0.8*X	## For plotting, histogram bins and simulation cutoff
 	ymax = 0.5
-	Nbin = 50
-	xbins = np.linspace(xmin,xmax,Nbin+1)
-	ybins = np.linspace(-ymax,ymax,Nbin+1)
-	dx = (xmax-x0)/Nbin; dy = 2*ymax/Nbin
+	Nxbin = 50
+	Nybin = 50
+	xbins = np.linspace(xmin,xmax,Nxbin+1)
+	ybins = np.linspace(-ymax,ymax,Nybin+1)
+	dx = (xmax-x0)/Nxbin; dy = 2*ymax/Nybin
 	
-	hisfile = "tempBHIS/BHIS_a"+str(a)+"_N"+str(Nbin)+"_r"+str(Nrun)
-	savedata = True
+	hisfile = "Pressure/BHIS/BHIS_a"+str(a)+"_Nx"+str(Nxbin)+"_r"+str(Nrun)
+	
+	if os.path.isfile(hisfile): return hisfile
 	
 	## ----------------------------------------------------------------
 	
 	## Precompute exp(-t) and initialise histogram
 	expmt = np.exp(-np.arange(0,tmax,dt))
-	H = np.zeros((Nbin,Nbin))
+	H = np.zeros((Nxbin,Nybin))
 	## Loop over initial y-position
 	for y0 in ybins:
 		if vb: print me+str(Nrun),"trajectories with y0 =",round(y0,2)
@@ -96,12 +106,13 @@ def main():
 			H += h*histogram_weight(y0,y[-1])
 	H = (H.T)[::-1]
 	## Normalise
-	H /= np.trapz(np.trapz(H,dx=dx,axis=1), dx=dy)
-	if savedata:	save_data(hisfile, H, vb)
+	H /= Nybin*Nrun
+	# H /= np.trapz(np.trapz(H,dx=dx,axis=1), dx=dy)
+	save_data(hisfile, H, vb)
 	
 	if vb: print me+"execution time",round(time.time()-t0,2),"seconds"
 	
-	return
+	return hisfile
 	
 ## ====================================================================
 def boundary_sim(x0y0, b, X, f, xmin, tmax, expmt=None, vb=False):
@@ -113,16 +124,14 @@ def boundary_sim(x0y0, b, X, f, xmin, tmax, expmt=None, vb=False):
 	If this turns out to not be the case, will have to dynamically increase array size.
 	-- Computing eta and then x. Maybe faster to have one loop.
 	"""
-
 	me = "LE_BoundarySimPlt.boundary_sim: "
+	t0 = time.time()
 	
 	## Initialisation
 	x0,y0 = x0y0
-	# seed = np.random.randint(1000000); np.random.seed(seed)
 	nstp = int(tmax/dt)
 	xi = np.random.normal(0, 1, nstp)
 	
-	t0 = time.time()
 	## OU noise
 	if expmt is None or expmt.shape[0]!=nstp: expmt = np.exp(-np.linspace(0,tmax,nstp))
 	y = y0*expmt + np.sqrt(2)*dt * fftconvolve(expmt,np.append(np.zeros(nstp),xi),"full")[nstp-1:-nstp]
@@ -135,7 +144,7 @@ def boundary_sim(x0y0, b, X, f, xmin, tmax, expmt=None, vb=False):
 	for i in xrange(1,nstp):
 		x[i] += x[i-1] + dt*(f(x[i-1],b,X) + y[i-1])
 		if x[i] < xmin:	break
-	if i==nstp-1: print me+"Trarray ran out of space. BUG!"
+	if i==nstp-1 and vb: print me+"Trarray ran out of space. BUG!"
 	if vb: print me+"Simulation of x  ",round(time.time()-t0,1),"seconds for",len(x),"steps"
 	
 	## Clip y and x to be their actual lengths
@@ -151,8 +160,13 @@ def histogram_weight(yi,yf):
 	Probability of y obeys Gaussian with zero mean and variance ???
 	"""
 	return np.exp(-(yi*yi+yf*yf)/(2.0))
-
 	
+## ====================================================================
+def calculate_xmax(X,a):
+	xmax = max([X*(1.0+0.02/(a*a*a)),1.02])
+	xmax = min([xmax,5.0])
+	return xmax
+
 ## ====================================================================
 ## ====================================================================
 if __name__=="__main__": main()
--- LE_Pressure.py
+++ LE_Pressure.py
@@ -2,9 +2,10 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from sys import argv
-import os
+import os, glob
 import optparse
 from time import time as sysT
+from LE_LightBoundarySim import calculate_xmax
 
 def main():
 	"""
@@ -30,62 +31,139 @@ def main():
 	EXAMPLE
 		python LE_Pressure.py dat_LE_stream\b=0.01\BHIS_y0.5bi50r5000b0.01X1seed65438.npy
 	
-	BUGS
+	BUGS / TODO
+		-- Honest normalisation -- affects pressure
 	
 	HISTORY
-		2015/11/12	Started
+		12 November 2015	Started
+		15 November 2015	Pressure versus alpha functionality
 	"""
-	me = "LE_Pressure: "
+	me = "LE_Pressure.main: "
 	t0 = sysT()
 	
 	## Options
-	parser = optparse.OptionParser()	
+	parser = optparse.OptionParser(conflict_handler="resolve")	
 	parser.add_option('-s','--show',
 		dest="showfig", default=False, action="store_true")
 	parser.add_option('-v','--verbose',
 		dest="verbose", default=False, action="store_true")
+	parser.add_option('-h','--help',
+                  dest="help",default=False,action="store_true",
+				  help="Print docstring.")		
 	opt = parser.parse_args()[0]
+	if opt.help: print main.__doc__; return
 	showfig = opt.showfig
 	verbose = opt.verbose
 	
-	## Need to WALK
-	# datafiles = np.sort(glob.glob(argv[1]+"/*.npy"))
+	argv[1] = argv[1].replace("\\","/")
+	if os.path.isfile(argv[1]):
+		pressure_pdf_plot_file(argv[1],verbose)
+	elif os.path.isdir(argv[1]):
+		pressure_plot_dir(argv[1],verbose)
+	else:
+		print me+"you gave me rubbish. Abort."
+		exit()
 	
-	filepath = argv[1].replace("\\","/")
+	if verbose: print me+"execution time",round(sysT()-t0,2),"seconds"
+	if showfig: plt.show()
+	
+	return
+	
+##=============================================================================
+def pressure_pdf_plot_file(filepath, verbose):
+	"""
+	Make plot for a single file
+	"""
+	me = "LE_Pressure.pressure_pdf_plot_file: "
+	t0 = sysT()
+	
+	## Filenames
+	plotfilePDF = "Pressure/"+os.path.splitext(os.path.split(filepath)[1])[0]+"_PDF.png"
 	plotfile = "Pressure/"+os.path.splitext(os.path.split(filepath)[1])[0]+"_P.png"
-
-	Hx = np.load(filepath).sum(axis=1)
-	xmin,xmax = 0.9,1.1
-	x  = np.linspace(xmin,xmax,Hx.shape[0])
-	Hx /= np.trapz(Hx,x=x)
 	
 	## Get alpha from filename
-	try:
-		start = filepath.find("b=")
-		alpha = float(filepath[start+2:filepath.find("/",start)])
-	except: alpha = 0.1
+	start = filepath.find("_a") + 2
+	alpha = float(filepath[start:filepath.find("_",start)])
 	if verbose: print me+"alpha =",alpha
 	
+	## Load data and find ranges
+	H = np.load(filepath)
+	Hx = H.sum(axis=0)
+	xmin,xmax = 0.8,calculate_xmax(1.0,alpha)
+	x  = np.linspace(xmin,xmax,Hx.shape[0])
+	# Hx /= np.trapz(Hx,x=x)
+	
+	## 2D PDF plot
+	if 1:
+		plt.imshow(H, extent=[xmin,xmax,-0.5,0.5], aspect="auto")
+		plot_acco(plt.gca(),xlabel="$x$",ylabel="$y$",title="$\\alpha=$"+str(alpha))
+		plt.savefig(plotfilePDF)
+		if verbose: print me+"plot saved to",plotfilePDF
+
+	## Calculate pressure
 	force = -alpha*0.5*(np.sign(x-1)+1)
 	press = - np.cumsum(force*Hx)
 	
 	fig,ax = plt.subplots(1,2)
 	ax[0].plot(x,Hx)
+	ax[0].set_xlim(left=0.9)
 	plot_acco(ax[0],ylabel="PDF")
 	ax[1].plot(x,press)
+	ax[1].set_xlim(left=0.9)
 	plot_acco(ax[1],ylabel="Pressure")
 	plt.tight_layout()
 	fig.suptitle("$\\alpha=$"+str(alpha),fontsize=16);plt.subplots_adjust(top=0.9)
 	
 	plt.savefig(plotfile)
 	if verbose: print me+"plot saved to",plotfile
+		
+	return plotfilePDF, plotfile
 	
-	print me+"execution time",round(sysT()-t0,2),"seconds"
-	if showfig: plt.show()
+##=============================================================================
+def pressure_plot_dir(dirpath, verbose):
+	"""
+	Plot pressure at "infinity" against alpha for all files in directory
+	"""
+	me = "LE_Pressure.pressure_plot_dir: "
+	t0 = sysT()
 	
+	## FIle discovery
+	histfiles = np.sort(glob.glob(dirpath+"/*.npy"))
+	numfiles = len(histfiles)
+	if verbose: print me+"found",numfiles,"files"
 	
-	return
+	## Outfile name
+	pressplot = dirpath+"/PressureAlpha.png"
+	Alpha = np.zeros(numfiles)
+	Press = np.zeros(numfiles)
 	
+	## Loop over files
+	for i,filepath in enumerate(histfiles):
+		
+		## Find alpha
+		start = filepath.find("_a") + 2
+		Alpha[i] = float(filepath[start:filepath.find("_",start)])
+		
+		## Load data and find ranges
+		Hx = np.load(filepath).sum(axis=0)
+		xmin,xmax = 0.8,calculate_xmax(1.0,Alpha[i])
+		x  = np.linspace(xmin,xmax,Hx.shape[0])
+		Hx /= np.trapz(Hx,x=x)
+
+		## Calculate pressure
+		force = -Alpha[i]*0.5*(np.sign(x-1)+1)
+		Press[i] = -np.sum(force*Hx)
+	
+	plt.plot(Alpha,Press,"bo")
+	plot_acco(plt.gca(), xlabel="$\\alpha$", ylabel="Pressure")
+	
+	plt.savefig(pressplot)
+	if verbose: print me+"plot saved to",pressplot
+	
+	return pressplot
+
+
+##=============================================================================
 ##=============================================================================
 def plot_acco(ax, **kwargs):
 	"""
@@ -102,8 +180,8 @@ def plot_acco(ax, **kwargs):
 	try: ax.set_ylabel(kwargs["ylabel"], fontsize=14)
 	except: pass
 	ax.grid(True)
-	try: ax.legend(loc=kwargs["legloc"], fontsize=11)
-	except KeyError: ax.legend(loc="best", fontsize=11)
+	# try: ax.legend(loc=kwargs["legloc"], fontsize=11)
+	# except KeyError: ax.legend(loc="best", fontsize=11)
 	return
 	
 ##=============================================================================
