229e7030bea33af52d66f57fdcc34430c4c77d9d
Allium <cato@nyu.edu>
2016-10-11 20:46:25 -0400

    Tidied choppy contour plot by smoothing. Changed interpolation routine.

--- LE_PDF3D.py
+++ LE_PDF3D.py
@@ -2,6 +2,7 @@ me0 = "LE_PDF3D"
 
 import numpy as np
 import scipy.interpolate
+import scipy.ndimage
 from sys import argv
 import os, optparse, glob, time
 
@@ -50,13 +51,13 @@ def input():
 	verbose = opt.verbose
 		
 	if os.path.isfile(args[0]):
-		plot_pdf3D_file(args[0], nosave, verbose)
+		plot_pdf3D(args[0], nosave, verbose)
 	elif (plotall and os.path.isdir(args[0])):
 		showfig = False
 		filelist = np.sort(glob.glob(args[0]+"/BHIS_*"+searchstr+"*.npy"))
 		if verbose: print me+"Found",len(filelist),"files."
 		for histfile in filelist:
-			plot_pdf3D_file(histfile, nosave, verbose)
+			plot_pdf3D(histfile, nosave, verbose)
 			plt.close()
 	else: raise IOError, me+"Check input."
 	
@@ -66,7 +67,7 @@ def input():
 	return
 
 ##=============================================================================
-def plot_pdf3D_file(histfile, nosave, vb):
+def plot_pdf3D(histfile, nosave, vb):
 	"""
 	Read in data for a single file and plot 3D PDF.
 	"""
@@ -81,39 +82,57 @@ def plot_pdf3D_file(histfile, nosave, vb):
 	bins = np.load(os.path.dirname(histfile)+"/BHISBIN"+os.path.basename(histfile)[4:-4]+".npz")
 	rbins  = bins["rbins"]
 	erbins = bins["erbins"]
-	epbins = bins["epbins"]
 	r = 0.5*(rbins[1:]+rbins[:-1])
 	etar = 0.5*(erbins[1:]+erbins[:-1])
-	etap = 0.5*(epbins[1:]+epbins[:-1])
 
 	## Load histogram, convert to normalised pdf
 	H = np.load(histfile)
-	H /= np.trapz(np.trapz(np.trapz(H,etap,axis=2),etar,axis=1),r,axis=0)
-	try: H = H.sum(axis=2)
-	except ValueError: pass
+	try:				H = H.sum(axis=2)	## If old file
+	except ValueError: 	pass
+	H /= np.trapz(np.trapz(H,etar,axis=1),r,axis=0)
 	## To get probability density rather than probability
 	rho = H / ( (2*np.pi)**2.0 * reduce(np.multiply, np.ix_(r,etar)) )
-
-	## Create grids
-	NGridPoints = 100
-	x, y = np.linspace(r[0],r[-1],NGridPoints), np.linspace(etar[0],etar[-1],NGridPoints)
+	
+	## Resample
+	npoints = 100
+	x, y = np.linspace(r[0],r[-1],npoints), np.linspace(etar[0],etar[-1],npoints)
 	X, Y = np.meshgrid(x, y)
-	Z = scipy.interpolate.RectBivariateSpline(r,etar,rho)(x,y,grid=True)
+#	Z = scipy.interpolate.RectBivariateSpline(r,etar,rho, s=0)(x,y,grid=True).T	## Slower
+	Z = scipy.ndimage.interpolation.zoom(rho,[float(npoints)/r.size,float(npoints)/etar.size],order=1).T
+
+#	x, y = r, etar
+#	X, Y = np.meshgrid(x, y)
+#	Z = rho.T
 
 	## Plotting
 	fig = plt.figure()
-	ax = fig.gca(projection='3d')
+	ax = fig.gca(projection="3d")
 
-	ax.plot_surface(X, Y, Z, rstride=5, cstride=5, alpha=0.2)
-	ax.contourf(X, Y, Z, zdir='z', offset=-0.2*Z.max(), cmap=cm.coolwarm)
-	ax.contourf(X, Y, Z, zdir='x', offset=0.0, 	cmap=cm.coolwarm)
-	ax.contourf(X, Y, Z, zdir='y', offset=etar[-1], cmap=cm.coolwarm)
+	ax.plot_surface(X, Y, Z, rstride=5, cstride=5, alpha=0.2, antialiased=True)
+	
+	xoff, yoff, zoff = X.min(), -0.1*Y.max(), -0.2*Z.max()
+	ax.contourf(X, Y, Z, zdir='z', offset=zoff,	cmap=cm.coolwarm, antialiased=True)	## 2D projection
+	ax.contourf(X, Y, Z, zdir='x', offset=xoff,	cmap=cm.coolwarm, antialiased=True)	## p(eta)
+#	ax.contourf(X, Y, Z, zdir='y', offset=yoff,	cmap=cm.coolwarm, antialiased=True)	## p(r)	## Too choppy
 
-	## Fit / prediction
-	# ax.plot(x, np.sqrt(a/(2*np.pi))*np.exp(-0.5*a*x*x), "g--", zdir="x")
+	## Plot smoothed p(r) contour
+	Zsm = scipy.ndimage.gaussian_filter(Z, sigma=2.0, order=0, mode="nearest")
+	ax.contourf(X, Y, Zsm, zdir='y', offset=yoff, cmap=cm.coolwarm, antialiased=True)
+	Q = np.trapz(Z.T*2*np.pi*y,y,axis=1)
+	ax.plot(x, yoff*np.ones(y.shape),Q/Q.max()*Z.max(), "r--",lw=3)	## p(r)
+
+	## Fit
+	ax.plot(xoff*np.ones(x.size), y, Z[5:,:].max()*np.exp(-0.5*a*y*y), "g--", lw=3, zorder=2)
+	## Wall
+	ax.plot(S*np.ones(x.size), y, 0.8*zoff*np.ones(x.size), "g--", lw=3, zorder=2)
+	ax.plot(R*np.ones(x.size), y, 0.8*zoff*np.ones(x.size), "g--", lw=3, zorder=2)
 
 	## Accoutrements
-	ax.set_zlim(-0.2*Z.max(),ax.get_zlim()[1])
+	ax.set_zlim(zoff,ax.get_zlim()[1])
+	ax.set_ylim(yoff,ax.get_ylim()[1])
+	ax.elev = 30
+	ax.azim = 45
+	
 	ax.set_xlabel(r"$r$", 		fontsize=18)
 	ax.set_ylabel(r"$\eta_r$", 	fontsize=18)
 	ax.set_zlabel(r"$\rho$", 	fontsize=18)
