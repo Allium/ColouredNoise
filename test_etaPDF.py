76ae14c4c126eba6861f07856d60900ae5c838a8
Captain <cato@nyu.edu>
2016-10-12 21:58:57 -0400

    Resurrected eta pdf code, now split into regions for comparison with pdf3d.

---
+++ test_etaPDF.py
@@ -0,0 +1,125 @@
+me0 = "test_etaPDF"
+
+import numpy as np
+import scipy as sp
+from scipy.optimize import curve_fit
+from matplotlib import pyplot as plt
+from sys import argv
+import os
+from LE_Utils import filename_par
+
+"""
+Plot pdf of eta if given a file. pdf split into three regions.
+"""
+
+try:
+
+	histfile = argv[1]
+
+	a = filename_par(histfile, "_a")
+	R = filename_par(histfile, "_R")
+	S = filename_par(histfile, "_S")
+	
+	bulk = False if R==S else True
+	
+	try: nosave = not bool(argv[2])
+	except IndexError: nosave = True
+
+	## Space (for axes)
+	bins = np.load(os.path.dirname(histfile)+"/BHISBIN"+os.path.basename(histfile)[4:-4]+".npz")
+	rbins = bins["rbins"]
+	rmax = rbins[-1]
+	r = 0.5*(rbins[1:]+rbins[:-1])
+	erbins = bins["erbins"]
+	er = 0.5*(erbins[1:]+erbins[:-1])
+	
+	## Wall indices
+	Rind = np.abs(r-R).argmin()
+	Sind = np.abs(r-S).argmin()
+		
+	## Load histogram, convert to normalised pdf
+	H = np.load(histfile)
+	try:	H = H.sum(axis=2)
+	except ValueError:	pass
+	
+	## Distribution on either side of the wall: inner, outer, bulk
+	rS = r[:Sind];		HS = H[:Sind,:]
+	rR = r[Rind:];		HR = H[Rind:,:]
+	if bulk:	rB = r[Sind:Rind];	HB = H[Sind:Rind,:]
+	## After the split, position is irrelevant
+	H  = np.trapz(H, r, axis=0)
+	HS = np.trapz(HS, rS, axis=0)
+	HR = np.trapz(HR, rR, axis=0)
+	if bulk:	HB = np.trapz(HB, rB, axis=0)
+	## rho is probability density
+	erho  = H/(2*np.pi*er) / np.trapz(H, er)
+	erhoS = HS/(2*np.pi*er) / np.trapz(HS, er)
+	erhoR = HR/(2*np.pi*er) / np.trapz(HR, er)
+	if bulk:	erhoB = HB/(2*np.pi*er) / np.trapz(HB, er)
+
+	##---------------------------------------------------------------			
+	## PLOT SET-UP
+		
+	figtit = "Density; $\\alpha="+str(a)+"$"
+	fig, ax = plt.subplots(1,1)
+	fsa = 16
+			
+	##---------------------------------------------------------------	
+	## PDF PLOT
+	
+	ax.plot(er,erho, "k-", label="Total")
+	ax.fill_between(er,0,erho,facecolor="black",alpha=0.1)
+	ax.plot(er,erhoS, "r-", label=r"Inner $S>r$")
+	ax.fill_between(er,0,erhoS,facecolor="red",alpha=0.1)
+	ax.plot(er,erhoR, "g-", label=r"Outer $r>R$")
+	ax.fill_between(er,0,erhoR,facecolor="green",alpha=0.1)
+	if bulk:
+		ax.plot(er,erhoB, "b-", label="Bulk $R>r>S$")
+		ax.fill_between(er,0,erhoB,facecolor="blue",alpha=0.1)
+
+	## Fit
+	er2 = np.linspace(er[0],er[-1]*(1+0.5/er.size),er.size*5+1)
+	fitfunc = lambda x, A, s2:\
+				A/(2*np.pi*s2)*np.exp(-0.5*x*x/s2)
+	fitT = sp.optimize.curve_fit(fitfunc, er, erho, p0=[1.0,1.0])[0]
+	fitS = sp.optimize.curve_fit(fitfunc, er, erhoS, p0=[1.0,1.0])[0]
+	fitR = sp.optimize.curve_fit(fitfunc, er, erhoR, p0=[1.0,1.0])[0]
+	if bulk:	fitB = sp.optimize.curve_fit(fitfunc, er, erhoB, p0=[1.0,1.0])[0]
+	else:		fitB = [0,0]
+	print [a,R,S],"\t s2 = ",np.around([fitT[1], fitS[1], fitR[1], fitB[1]],3)
+	# ax.plot(er2, fitfunc(er2, *fit), "g--", lw=2)
+
+	## Accoutrements
+	ax.set_xlim(left=0.0)
+	ax.set_xlabel(r"$\eta$", fontsize=fsa)
+	ax.set_ylabel(r"$\rho(\eta,\phi)$", fontsize=fsa)
+	ax.grid()
+	ax.legend()
+
+	plt.title(r"PDF of $\eta$, divided into regions. $\alpha="+str(a)+"$, $R="+str(R)+"$, $S="+str(S)+"$")
+	
+	if not nosave:
+		plotfile = os.path.dirname(histfile)+"/PDFeta"+os.path.basename(histfile)[4:-4]+".jpg"
+		fig.savefig(plotfile)
+		print me0+": Figure saved to",plotfile
+	plt.show()
+	exit()
+
+## =========================================================================================
+
+except IndexError:
+
+	a = np.array([0.2,0.4,0.5,0.7,1.0,1.5,2.0,2.5,3.0,5.0,10.0])
+
+	s2 = np.array([5.25150854,2.55776206,2.04659908,1.43965591,1.01097344,0.67096949,0.50612912,0.39905598,0.33493162,0.20093551,0.10061558])
+
+	plt.plot(a, s2, "bo-", label=r"$\sigma^2$")
+	plt.plot(a, a**(-1.), "k--", label=r"$1/\alpha$")
+
+	plt.xlabel(r"$\alpha$")
+	plt.xscale("log")
+	plt.yscale("log")
+	plt.legend(loc="best")
+	plt.grid()
+
+	plt.show()
