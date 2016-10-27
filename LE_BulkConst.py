FPEr: corrected error with computation of rho; now agrees between _psi and 2D files. LE_SBS temporarily modified to output psi in H. Otherwise small changes / tidying

LE_BulkConst.py
@@ -255,11 +255,9 @@ def bulk_const(histfile):
 		r = 0.5*(rbins[1:]+rbins[:-1])
 		etar = 0.5*(erbins[1:]+erbins[:-1])
 		## For old _phi files
-		try:
+		if bool(histfile.find("_phi")+1):
 			epbins = bins["epbins"]
 			H = H.sum(axis=2) * (epbins[1]-epbins[0])
-		except KeyError:
-			pass
 		
 		## Probability
 		## Normalise
@@ -279,7 +277,8 @@ def bulk_const(histfile):
 		c1 = np.trapz(rho * etar*etar * 2*np.pi*etar, etar, axis=1)
 		## Calculate average eta
 		er2E = c1 / (Q+(Q==0))	  ## Avoid /0 warning (numerator is 0 anyway)
-	
+
+#	## Plot rho eta^2, in bulk region, on r-etar plane.
 #	X, Y = np.meshgrid(r, etar)
 #	Z = rho * etar*etar / Q[:,np.newaxis] #* 2*np.pi*etar
 #	Z[X.T>R]=0.0; Z[X.T<S]=0.0

LE_SBS.py
@@ -179,7 +179,8 @@ def main(a,ftype,fpar,Nrun,dt,timefac,intmeth,ephi,vb):
 	if ephi:	
 		Nepbin = 50
 		epbins = np.linspace(0.0,2*np.pi,Nepbin+1)
-		pstr = "_phi"
+		# pstr = "_phi"
+		pstr = "_psi"
 		bins = [rbins,erbins,epbins]	
 	else:
 		pstr = ""
@@ -335,7 +336,7 @@ def boundary_sim(xyini, exyini, a, xy_step, dt, tmax, expmt, ephi, vb):
 	## -----------------===================-----------------
 			
 	if ephi:
-		epcoord = np.arctan2(exy[:,1],exy[:,0])
+		epcoord = np.arctan2(exy[:,1],exy[:,0]) - np.arctan2(xy[:,1],xy[:,0])
 		return [rcoord, ercoord, epcoord]
 	else:
 		return [rcoord, ercoord]

test_FPEr.py
@@ -5,6 +5,11 @@ import scipy.interpolate, scipy.ndimage, scipy.optimize
 from sys import argv
 import os, optparse, glob, time
 from matplotlib import cm
+
+if "SSH_TTY" in os.environ:
+	print me0+": Using Agg backend."
+	import matplotlib as mpl
+	mpl.use("Agg")
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable as mplmal
 
@@ -15,8 +20,13 @@ from LE_Utils import filename_par
 import warnings
 warnings.filterwarnings("ignore",category=FutureWarning)
 
+fsa, fsl, fst = 18, 12, 14
+
 """
-Does the derived density in r and etar satisfy the radial FPE derived in 23/09/2016
+Does the derived density in r and etar satisfy the radial FPE.
+Incorrect derivation 23/09/2016; more correct 25/10/2016.
+
+py test_FPEr.py filename opts
 """
 
 ##=============================================================================
@@ -67,51 +77,75 @@ def plot_FPEres(histfile, nosave, vb):
 	"""
 	Read in data for a single file density, run it through FP operator, and plot.
 	"""
-	me = me0+"plot_FPEres: "
+	me = me0+".plot_FPEres: "
 
 	## Get pars from filename
 	a = filename_par(histfile, "_a")
 	R = filename_par(histfile, "_R")
 	S = filename_par(histfile, "_S")
+	
+	phifile = bool(histfile.find("_phi") + 1)
+	psifile = bool(histfile.find("_psi") + 1)
 				
+	## ------------------------------------------------------------------------
 	## Space (for axes)
 	bins = np.load(os.path.dirname(histfile)+"/BHISBIN"+os.path.basename(histfile)[4:-4]+".npz")
 	rbins  = bins["rbins"]
 	erbins = bins["erbins"]
 	r = 0.5*(rbins[1:]+rbins[:-1])
 	etar = 0.5*(erbins[1:]+erbins[:-1])
-
-	## Load histogram
-	H = np.load(histfile)
-	try:				H = H.sum(axis=2)	## If old _phi file
-	except ValueError: 	pass
-	
-	## ------------------------------------------------------------------------
-	
-	## Normalise and convert to density
-	H /= np.trapz(np.trapz(H,etar,axis=1),r,axis=0)
-	rho = H / ( (2*np.pi)**2.0 * reduce(np.multiply, np.ix_(r,etar)) )
-	
-	## ------------------------------------------------------------------------
+	if psifile:
+		epbins = bins["epbins"]
+		etap = 0.5*(epbins[1:]+epbins[:-1])
 
 	## Force
 	assert histfile.find("_DL_")
 	ftype = "dlin"
-	f = force_dlin(r,r,R,S)
-		
-	##
-	rr = r[:,np.newaxis]
-	ee = etar[np.newaxis,:]
-	ff = f[:,np.newaxis]
-		
-	## FPE operator ARRAY
-	D = lambda arr, x, **kwargs: scipy.ndimage.gaussian_filter1d(arr, 1.0, axis=kwargs["axis"], order=1) / (x[1]-x[0])
-	Drrho = scipy.ndimage.gaussian_filter1d(rho, 1.0, axis=0, order=1) / (r[1]-r[0])
-	Derho = scipy.ndimage.gaussian_filter1d(rho, 1.0, axis=1, order=1) / (etar[1]-etar[0])
-	DDerho = scipy.ndimage.gaussian_filter1d(rho, 1.0, axis=1, order=2) / (etar[1]-etar[0])**2
-	res = -D((ee+ff)*rho, r, axis=0) -1/rr*(ee+ff)*rho + 1/a*D(ee*rho, etar, axis=1) + 1/a*rho +\
-			+ 1/a**2*1/ee*Derho + 1/a**2*DDerho#D(D(rho,etar,axis=1),etar,axis=1)
+	f = force_dlin(r,r,R,S)			
+				
+	## Spatial arrays with dimensions commensurate to rho
+	if psifile:
+		ff = f[:,np.newaxis,np.newaxis]
+		rr = r[:,np.newaxis,np.newaxis]
+		ee = etar[np.newaxis,:,np.newaxis]
+		pp = etap[np.newaxis,np.newaxis,:]
+		dV = (r[1]-r[0])*(etar[1]-etar[0])*(etap[1]-etap[0])	## Assumes regular grid
+	else:
+		ff = f[:,np.newaxis]
+		rr = r[:,np.newaxis]
+		ee = etar[np.newaxis,:]
+		dV = (r[1]-r[0])*(etar[1]-etar[0])	## Assumes regular grid
+	
+	## ------------------------------------------------------------------------
 
+	## Load histogram
+	H = np.load(histfile)
+	if phifile:
+		H = H.sum(axis=2)	## If old _phi file
+	
+	## Normalise and convert to density
+	H /= H.sum()*dV
+	rho = H / ( (2*np.pi)**2.0 * rr*ee )
+		
+	## ------------------------------------------------------------------------
+		
+	## Derivative function
+	D = lambda arr, x, order, **kwargs: \
+			scipy.ndimage.gaussian_filter1d(arr, 1.0, order=order, axis=kwargs["axis"]) / (x[1]-x[0])
+	
+	## FPE operator
+	t0 = time.time()
+	if psifile:
+		res = -D((ee*np.cos(pp)+ff)*rho, r, 1, axis=0) -1/rr*ff*rho + 1/rr*ee*np.sin(pp)*D(rho, etap, 1, axis=2) +\
+				+ 1/a*D(ee*rho, etar, 1, axis=1) + 1/a*rho +\
+				+ 1/a**2*1/ee*D(rho, etar, 1, axis=1) + 1/a**2*D(rho, etar, 2, axis=1) + 1/a**2*1/ee**2*D(rho, etap, 2, axis=2)
+	else:
+		## If radial (r,eta) only, or (r,eta,eta_phi) converted to (r,eta)
+		res = -D((ee+ff)*rho, r, 1, axis=0) -1/rr*(ee+ff)*rho + 1/a*D(ee*rho, etar, 1, axis=1) +\
+				+ 1/a*rho + 1/a**2*1/ee*D(rho, etar, 1, axis=1) +\
+				+ 1/a**2*D(rho, etar, 2, axis=1)
+	if vb: print me+"Residue calculation %.2g seconds."%(time.time()-t0)
+			
 	## ------------------------------------------------------------------------
 
 	## Plotting
@@ -119,37 +153,53 @@ def plot_FPEres(histfile, nosave, vb):
 
 	X, Y = np.meshgrid(r, etar)
 	
+	## ------------------------------------------------------------------------
+
 	## Plot density
 	ax = axs[0]
-	Z = rho.T #/ rho.max()
-	im = ax.contourf(X, Y, Z, 10, vmin=0.0, antialiased=True)
+	if psifile:
+		Z = np.trapz(rho, etap, axis=2).T
+	else:
+		Z = rho.T
+	
+	clim = float("%.1g"%(Z.max()))
+	im = ax.contourf(X, Y, Z, levels=np.linspace(0,clim,11), vmin=0.0, antialiased=True)
 	cax = mplmal(ax).append_axes("right", size="5%", pad=0.05)
-	fig.colorbar(im, cax=cax)\
+	fig.colorbar(im, cax=cax)
 	
-	axs[0].set_ylabel(r"$\eta_r$", 	fontsize=18)
-	ax.set_title("2D Radial Density", fontsize=14)
+	axs[0].set_ylabel(r"$\eta_r$", 	fontsize=fsa)
+	ax.set_title(r"2D Radial Density. $\alpha=%.1f$, $R=%.1f$, $S=%.1f$."%(a,R,S), fontsize=fst)
 	
+	## ------------------------------------------------------------------------
+
 	## Plot residue
 	ax = axs[1]
-	Z = res.T #/ np.abs(res).max()
-	clim = float("%.1g"%(Z.mean()+2*Z.std()))
+	if psifile:
+		Z = np.trapz(res, etap, axis=2).T
+	else:
+		Z = res.T
+	clim = float("%.1g"%(Z.mean()+1.0*Z.std()))	## std dominated by choppy r=0.
 	im = ax.contourf(X, Y, Z, levels=np.linspace(-clim,clim,11), cmap=cm.BrBG, antialiased=True)
 	cax = mplmal(ax).append_axes("right", size="5%", pad=0.05)
 	fig.colorbar(im, cax=cax)
 	
-	axs[1].set_xlabel(r"$r$", 		fontsize=18)
-	axs[1].set_ylabel(r"$\eta_r$", 	fontsize=18)
-	ax.set_title("Radial FP Operator On Density", fontsize=14)
+	axs[1].set_xlabel(r"$r$", 		fontsize=fsa)
+	axs[1].set_ylabel(r"$\eta_r$", 	fontsize=fsa)
+	ax.set_title("Radially Symmetric FP Residual", fontsize=fst)
+	
+	## ------------------------------------------------------------------------
 	
 	## Indicate wall
 	for ax in axs:
 		ax.axvline(S, c="k", lw=2)
 		ax.axvline(R, c="k", lw=2)
 
+	## ------------------------------------------------------------------------
+
 	if not nosave:
-		plotfile = os.path.dirname(histfile)+"/FPEres"+os.path.basename(histfile)[4:-4]+".jpg"
+		plotfile = os.path.dirname(histfile)+"/FPEres"+os.path.basename(histfile)[4:-4]+"_psi"*psifile+".jpg"
 		fig.savefig(plotfile)
-		if vb:	print me+": Figure saved to",plotfile
+		if vb:	print me+"Figure saved to",plotfile
 
 	return
 	