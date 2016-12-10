Two bugs (bad normalisation and bad STind assignment) in CBC now fixed.

LE_CBulkConst.py
@@ -108,6 +108,9 @@ def plot_file(histfile, nosave, vb):
 	ax.plot(x, Q/Q.max(),   label=r"$Q(x)$")
 	ax.plot(x, ex2/ex2.max(), label=r"$\langle\eta_x^2\rangle(x)$")
 	ax.plot(x, BC/BC.max(), label=r"$\langle\eta_x^2\rangle Q$", lw=2)
+#	ax.plot(x, Q,   label=r"$Q(x)$")
+#	ax.plot(x, ex2, label=r"$\langle\eta_x^2\rangle(x)$")
+#	ax.plot(x, BC, label=r"$\langle\eta_x^2\rangle Q$", lw=2)
 	
 	ax.plot(x, U/U.max()*ax.get_ylim()[1], "k--", label=r"$U(x)$")	
 		
@@ -118,6 +121,11 @@ def plot_file(histfile, nosave, vb):
 	elif "_ML_" in histfile:
 		ax.axvspan(S,R, color="yellow",alpha=0.2)
 		ax.axvspan(-R,T, color="yellow",alpha=0.2)
+		ax.axvline(S, c="k",lw=2);	ax.axvline(R, c="k",lw=2)
+		ax.axvline(T, c="k",lw=2);	ax.axvline(-R, c="k",lw=2)
+	elif "_CL_" in histfile:
+		ax.axvspan(S,R, color="yellow",alpha=0.2)
+		ax.axvspan(0,T, color="yellow",alpha=0.2)
 		ax.axvline(S, c="k",lw=2);	ax.axvline(R, c="k",lw=2)
 		ax.axvline(T, c="k",lw=2);	ax.axvline(-R, c="k",lw=2)
 		
@@ -136,6 +144,7 @@ def plot_file(histfile, nosave, vb):
 	fig.suptitle(title,fontsize=fs["fst"])
 	
 	## SAVE
+#	ax.set_ylim(top=BC.max())
 	plotfile = os.path.dirname(histfile)+"/QEe2"+os.path.basename(histfile)[4:-4]+"."+fs["saveext"]
 	if not nosave:
 		fig.savefig(plotfile)
@@ -152,7 +161,6 @@ def plot_dir(histdir, nosave, srchstr, vb):
 	(where applicable) and plot against alpha.
 	"""
 	me = me0+".plot_dir: "
-	print me+"Not working for some reason."
 	
 	filelist = np.sort(glob.glob(histdir+"/BHIS_CAR_*"+srchstr+"*.npy"))
 	numfiles = filelist.size
@@ -177,12 +185,12 @@ def plot_dir(histdir, nosave, srchstr, vb):
 		
 		## Wall indices
 		Rind, Sind, Tind = np.abs(x-R).argmin(), np.abs(x-S).argmin(), np.abs(x-T).argmin()
-		STind = (Tind+Sind)/2
+		STind = 0 if "_DL_" in histfile else (Tind+Sind)/2
 		
 		##---------------------------------------------------------------------
 		## Calculate pressure from BC
 		
-		if   "_DL_" in histfile:	
+		if "_DL_" in histfile:	
 			BCsr = BC[Sind:Rind+1].mean()
 			pR[i] = A[i] * BCsr
 			pS[i] = A[i] * BCsr
@@ -200,8 +208,8 @@ def plot_dir(histdir, nosave, srchstr, vb):
 			BCts = BC[STind]
 			BCrt = BC[x.size-Rind:Tind+1].mean()
 			pR[i] = A[i] * BCsr
-			pS[i] = A[i] * (BCsr - BCts)
-			pT[i] = A[i] * (BCrt - BCts)
+			pS[i] = A[i] * (-BCsr + BCts)
+			pT[i] = A[i] * (-BCrt + BCts)
 			
 		elif "_NL_" in histfile:
 			BCr = BC[Rind]
@@ -245,7 +253,7 @@ def plot_dir(histdir, nosave, srchstr, vb):
 	PT_WN = -sp.integrate.trapz(fx[Tind:STind]*Qx_WN[Tind:STind], x[Tind:STind])
 	
 	## Normalise
-#	pR /= PR_WN; pS /= PS_WN; pT /= PT_WN
+	pR /= PR_WN; pS /= PS_WN; pT /= PT_WN
 	PR /= PR_WN; PS /= PS_WN; PT /= PT_WN
 	
 	##-------------------------------------------------------------------------
@@ -272,7 +280,7 @@ def plot_dir(histdir, nosave, srchstr, vb):
 	ax.set_xlim(0.0,A[-1])
 	
 	ax.set_xlabel(r"$\alpha$",fontsize=fs["fsa"])
-	ax.set_ylabel(r"$P$",fontsize=fs["fsa"])
+	ax.set_ylabel(r"$P(\alpha)$",fontsize=fs["fsa"])
 	ax.grid()
 	ax.legend(loc="best", fontsize=fs["fsl"]).get_frame().set_alpha(0.5)
 	title = "Pressure normalised by WN result. $R=%.1f, S=%.1f, T=%.1f.$"%(R,S,T) if T>=0.0\
@@ -280,8 +288,8 @@ def plot_dir(histdir, nosave, srchstr, vb):
 	fig.suptitle(title,fontsize=fs["fst"])
 	
 	## SAVING
-	plotfile = histdir+"/QEe2_Pa_R%.1f_S%.1f_T%.1f.jpg"%(R,S,T) if T>=0.0\
-				else histdir+"/QEe2_Pa_R%.1f_S%.1f.jpg"%(R,S)
+	plotfile = histdir+"/QEe2_Pa_R%.1f_S%.1f_T%.1f"%(R,S,T) if T>=0.0\
+				else histdir+"/QEe2_Pa_R%.1f_S%.1f"%(R,S)
 	plotfile += "."+fs["saveext"]
 	if not nosave:
 		fig.savefig(plotfile)
@@ -311,7 +319,7 @@ def bulk_const(histfile):
 	
 	## Load histogram
 	H = np.load(histfile)
-	rhoxex = H.sum(axis=2) / (H.sum()*(x[1]-x[0])*(ex[1]-ex[0])*(ey[1]-ey[0]))
+	rhoxex = H.sum(axis=2) / (H.sum()*(x[1]-x[0])*(ex[1]-ex[0]))
 	
 	## Spatial density
 	Q = H.sum(axis=2).sum(axis=1) / (H.sum()*(x[1]-x[0]))

LE_CPDF.py
@@ -95,6 +95,7 @@ def plot_pdf1d(histfile, nosave, vb):
 	except ValueError: T= -S
 	
 	doQfit = (R==S and "_DL_" in histfile)
+	plotq = int(False)
 	
 	##-------------------------------------------------------------------------
 		
@@ -132,11 +133,11 @@ def plot_pdf1d(histfile, nosave, vb):
 	
 	## PLOTTING
 				
-	fig, axs = plt.subplots(2,1, figsize=fs["figsize"])
+	fig, axs = plt.subplots(1+plotq,1, figsize=fs["figsize"])
 	fig.canvas.set_window_title("1D PDFs")
 	
 	## Spatial density plot
-	ax = axs[0]
+	ax = axs[0] if plotq else axs
 	
 	## Data
 	ax.plot(x, Qx, label=r"Simulation")
@@ -153,18 +154,20 @@ def plot_pdf1d(histfile, nosave, vb):
 	else: raise IOError, me+"Force not recognised."
 	U = -sp.integrate.cumtrapz(fx, x, initial=0.0); U -= U.min()
 	
+#	ax.set_ylim((0.0,1.2))	###
 	ax.plot(x, np.exp(-U)/np.trapz(np.exp(-U),x), "r-", label="WN")
 	ax.plot(x, U/U.max()*ax.get_ylim()[1], "k--",label="Potential")
 	
 	## Indicate bulk
-	ax.axvline(S,c="k")
-	ax.axvline(R,c="k")
+	ax.axvline(S,c="k",lw=1)
+	ax.axvline(R,c="k",lw=1)
 	if T>=0.0:
 		ax.axvspan(S,R,color="y",alpha=0.1)
-		ax.axvline(T,c="k")
-		ax.axvspan(0,T,color="y",alpha=0.1)
+		ax.axvline(T,c="k",lw=1)
+		ax.axvspan(-R,T,color="y",alpha=0.1)
+		ax.axvline(-R,c="k",lw=1)
 	elif T<0.0:
-		ax.axvline(-R,c="k")
+		ax.axvline(-R,c="k",lw=1)
 	
 	ax.set_xlim(left=x[0],right=x[-1])
 	ax.set_xlabel(r"$x$", fontsize=fs["fsa"])
@@ -174,27 +177,34 @@ def plot_pdf1d(histfile, nosave, vb):
 		
 	##-------------------------------------------------------------------------
 	
-	## Force density plot
-	ax = axs[1]
+	if plotq:
+		## Force density plot
+		ax = axs[1]
 	
-	## Data
-	ax.plot(etax, qx, label=r"Simulation $x$")
-	ax.plot(etay, qy, label=r"Simulation $y$")
+		## Data
+		ax.plot(etax, qx, label=r"Simulation $x$")
+		ax.plot(etay, qy, label=r"Simulation $y$")
 	
-	## Gaussian
-	ax.plot(etax, gauss(etax,0.0,1/a), "c-", label=r"$G\left(0, \frac{1}{\alpha}\right)$")
+		## Gaussian
+		ax.plot(etax, gauss(etax,0.0,1/a), "c-", label=r"$G\left(0, \frac{1}{\alpha}\right)$")
 	
-	ax.set_xlabel(r"$\eta$", fontsize=fs["fsa"])
-	ax.set_ylabel(r"$q(\eta)$", fontsize=fs["fsa"])
-	ax.grid()
-	ax.legend(loc="upper right", fontsize=fs["fsl"]).get_frame().set_alpha(0.5)
+		ax.set_xlabel(r"$\eta$", fontsize=fs["fsa"])
+		ax.set_ylabel(r"$q(\eta)$", fontsize=fs["fsa"])
+		ax.grid()
+		ax.legend(loc="upper right", fontsize=fs["fsl"]).get_frame().set_alpha(0.5)
 	
-	##-------------------------------------------------------------------------
+		##-------------------------------------------------------------------------
 	
-	fig.tight_layout()
-	fig.subplots_adjust(top=0.95)
-	title = r"PDFs in $r$ and $\eta$. $\alpha=%.1f, R=%.1f, S=%.1f$"%(a,R,S)  if T<0.0\
-			else r"PDFs in $r$ and $\eta$. $\alpha=%.1f, R=%.1f, S=%.1f, T=%.1f$"%(a,R,S,T)
+		fig.tight_layout()
+		fig.subplots_adjust(top=0.95)
+		title = r"PDFs in $r$ and $\eta$. $\alpha=%.1f, R=%.1f, S=%.1f$"%(a,R,S)  if T<0.0\
+				else r"PDFs in $r$ and $\eta$. $\alpha=%.1f, R=%.1f, S=%.1f, T=%.1f$"%(a,R,S,T)
+				
+	else:			
+		title = r"Spatial PDF. $\alpha=%.1f, R=%.1g, S=%.1g$"%(a,R,S)  if T<0.0\
+				else r"Spatial PDF. $\alpha=%.1f, R=%.1f, S=%.1f, T=%.f$"%(a,R,S,T)
+				
+				
 	fig.suptitle(title, fontsize=fs["fst"])
 	
 	if not nosave:
@@ -285,7 +295,12 @@ def plot_pdf2d(histfile, nosave, vb):
 	
 	ax = axs[0][0] if pred else axs[0]
 	ax.contourf(x,etax,rhoxex.T)
-	ax.axvline(R,c="k"); ax.axvline(S,c="k")
+	
+	## Indicate bulk
+	ax.axvline(S,c="k",lw=1)
+	ax.axvline(R,c="k",lw=1)
+	if T>=0.0:	ax.axvline(T,c="k",lw=1)
+	elif T<0.0:	ax.axvline(-R,c="k",lw=1)
 	
 	ax.set_xlabel(r"$x$", fontsize=fs["fsa"])
 	ax.set_ylabel(r"$\eta_x$", fontsize=fs["fsa"])
@@ -303,7 +318,12 @@ def plot_pdf2d(histfile, nosave, vb):
 	
 	ax = axs[1][0] if pred else axs[1]
 	ax.contourf(x,etay,rhoxey.T)
-	ax.axvline(R,c="k"); ax.axvline(S,c="k")
+	
+	## Indicate bulk
+	ax.axvline(S,c="k",lw=1)
+	ax.axvline(R,c="k",lw=1)
+	if T>=0.0:	ax.axvline(T,c="k",lw=1)
+	elif T<0.0:	ax.axvline(-R,c="k",lw=1)
 	
 	ax.set_xlabel(r"$x$", fontsize=fs["fsa"])
 	ax.set_ylabel(r"$\eta_y$", fontsize=fs["fsa"])
@@ -354,32 +374,6 @@ def plot_pdf2d(histfile, nosave, vb):
 	return
 	
 	
-##=============================================================================
-##=============================================================================
-
-def calc_rho_NLsmallR(x, a, R, S):
-	"""
-	Calculate 1o solution for the small-R nlin scenario.
-	See notes 22/11/2016.
-	"""
-	Rind, Sind = np.abs(x-R).argmin(), np.abs(x-S).argmin()
-	DD = np.zeros(x.size); DD[Sind] = 1.0 / (x[1]-x[0])
-	return
-
-def calc_Q_NLsmallR(x, a, R, S):
-	"""
-	Calculate 1o spatial density for the small-R nlin scenario.
-	See notes 22/11/2016.
-	"""
-	Rind, Sind = np.abs(x-R).argmin(), np.abs(x-S).argmin()
-	DD = np.zeros(x.size); DD[Sind] = 1.0 #/ (x[1]-x[0])
-	Q0R = np.sqrt((a+1)/(2*np.pi))*np.exp(-0.5*(a+1)*(x-R)**2)
-	Q0L = np.sqrt((a+1)/(2*np.pi))*np.exp(-0.5*(a+1)*(x+R)**2)
-	n = 1/(1-2*R*np.sqrt((a+1)/(2*np.pi))*np.exp(-0.5*(a+1)*S*S))	## Maybe wrong
-	QR = n*(1 + R*(a+1)*x - 2*R*DD)*Q0R
-	QL = n*(1 - R*(a+1)*x - 2*R*DD)*Q0L
-	Q = np.r_[QL[:Sind],QR[Sind:]]
-	return Q
 
 ##=============================================================================
 ##=============================================================================

LE_CPressure.py
@@ -105,6 +105,9 @@ def plot_pressure_file(histfile, nosave, vb):
 	Rind, Sind, Tind = np.abs(x-R).argmin(), np.abs(x-S).argmin(), np.abs(x-T).argmin()
 	STind = (Sind+Tind)/2
 	
+	## Adjust indices for pressure calculation
+	if "_DL_" in histfile:
+		STind = 0
 	if "_NL_" in histfile:
 		STind = Sind
 		Sind = Rind
@@ -258,6 +261,14 @@ def plot_pressure_dir(histdir, srchstr, logplot, nosave, vb):
 		## Wall indices
 		Rind, Sind, Tind = np.abs(x-R).argmin(), np.abs(x-S).argmin(), np.abs(x-T).argmin()
 		STind = (Sind+Tind)/2
+	
+		## Adjust indices for pressure calculation
+		if "_DL_" in histfile:
+			STind = 0
+		if "_NL_" in histfile:
+			STind = Sind
+			Sind = Rind
+			Tind = x.size-Rind
 		
 		##-------------------------------------------------------------------------
 		
@@ -308,7 +319,7 @@ def plot_pressure_dir(histdir, srchstr, logplot, nosave, vb):
 	
 	## PLOTTING
 	
-	plotfile = histdir+"/PA_R%.1f_S%.1f_T%.1f."%(R,S,T)+fs["saveext"] if T>=0.0\
+	plotfile = histdir+"/PA_R%.1f_S%.ff_T%.1f."%(R,S,T)+fs["saveext"] if T>=0.0\
 				else histdir+"/PA_R%.1f_S%.1f."%(R,S)+fs["saveext"]
 	
 	fig, ax = plt.subplots(1,1, figsize=fs["figsize"])
@@ -320,8 +331,10 @@ def plot_pressure_dir(histdir, srchstr, logplot, nosave, vb):
 		
 	if logplot:
 		ax.set_xscale("log"); ax.set_yscale("log")
-		plotfile += "_loglog"
+		ax.set_xlim((ax.get_xlim()[0],A[-1]))
+		plotfile = plotfile[:-4]+"_loglog."+fs["saveext"]
 	else:
+		ax.set_xlim((0.0,A[-1]))
 		ax.set_ylim(bottom=0.0,top=max(ax.get_ylim()[1],1.0))
 	
 	ax.set_xlabel(r"$\alpha$", fontsize=fs["fsa"])
@@ -329,7 +342,7 @@ def plot_pressure_dir(histdir, srchstr, logplot, nosave, vb):
 	ax.grid()
 	ax.legend(loc="lower left", fontsize=fs["fsl"]).get_frame().set_alpha(0.5)
 	title = r"Pressure as a function of $\alpha$ for $R=%.1g,S=%.1g,T=%.1g$"%(R,S,T) if T>=0.0\
-			else r"Pressure as a function of $\alpha$ for $R=%.1g,S=%.1g$"%(R,S)
+			else r"Pressure as a function of $\alpha$ for $R=%.2g,S=%.2g$"%(R,S)
 	fig.suptitle(title, fontsize=fs["fst"])
 	
 	if not nosave: